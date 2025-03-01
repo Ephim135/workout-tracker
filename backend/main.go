ee7ed4442d86a16c6fc6e228b5ea1fe7c9fe98f0
Ephim135 <fabian009.berndt@gmail.com>
2025-03-01 10:04:59 +0100

    connecting front and backend

--- backend/main.go
+++ backend/main.go
@@ -4,13 +4,38 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
+
+type LoginRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
 
 func main() {
 	app := fiber.New()
+	app.Use(cors.New())
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Workout Tracker API")
+	})
+
+	app.Get("/api/message", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"message": "Hello from the backend!",
+		})
+	})
+
+	app.Post("/api/login", func(c *fiber.Ctx) error {
+		var req LoginRequest
+
+		if err := c.BodyParser(&req); err != nil {
+			return c.Status(400).JSON(fiber.Map{"error": "Invalid request"})
+		}
+
+		// authentication Logic
+
+		return c.Status(401).JSON(fiber.Map{"error": "Invalid credentials"})
 	})
 
 	log.Fatal(app.Listen(":3000"))
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,25 @@ import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import About from './components/About';
 import Home from './components/Home';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './styles.css';
 
 const App: React.FC = () => {
+  const [message, setMessage] = useState<string>('');
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:3000/api/message')
+      .then((response) => response.json())
+      .then((data) => setMessage(data.message))
+      .catch((error) => console.error('Error fetching message:', error));
+  }, [])
+
   return (
     <Router>
       <Navbar />
+      <div className="container">
+        <p>Backend says: {message}</p>
+      </div>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
