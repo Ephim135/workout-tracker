649b0c3bb0ca3a73c3b8ad483cfef92a6ef6d139
Ephim135 <fabian009.berndt@gmail.com>
2025-04-23 21:08:59 +0200

    add new svg

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -4,7 +4,15 @@ import LoginRegister from "./LoginRegister";
 const Navbar = () => {
   return (
     <nav className="relative mt-2 mb-6 flex h-16 items-center justify-between rounded bg-gray-500 px-6">
-      <a className="text-xl font-bold">LOGO</a>
+      <a className="flex items-center text-xl font-bold">
+        LOGO
+        <img
+          className="ml-2 inline"
+          src="src/assets/gym.svg"
+          width="42"
+          height="42"
+        />
+      </a>
       <div className="absolute top-1/2 left-1/2 flex -translate-x-1/2 -translate-y-1/2 space-x-6 text-xl font-bold">
         <Link className="hover:underline" to={"/"}>
           Home
